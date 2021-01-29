153195f153990455f195ea1104a83ab8d22a9364
Awushie.Fayose <Z006DTG@c02zw6bzmd6r.target.com>
2021-01-29 14:35:01 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,47 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Six Degrees of Drake</title>
+     <link rel="stylesheet" href="css/style.css">
+    </head> */}
+    {/* <body> */}
+        <div class="container">
+            <header>
+                <section>
+                        <h1>Six Degrees</h1> 
+                            <h1>of</h1>
+                            <h1>Drake</h1>
+                </section>
+            </header>
+            <section id="main">
+                <h2>How close is your favourite artist to Drizzy?</h2>
+            </section>
+            <section id="search">
+                <form action="">
+                    <input />
+                    <input type="button" value="search"/>
+                    <a class="button" href="/random"></a>
+                </form>
+            </section>
+            <section>
+                <button>
+                    <h2>RANDOM</h2>
+                </button>
+                Random button placeholder
+            </section>
+            <section>
+                Socials Placeholder
+            </section>
+            <footer>
+                &copy; Awushie Fayose LLC
+            </footer>
+        </div>
+    {/* </body> */}
+    </>
   );
 }
 
